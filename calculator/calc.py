7b74df5c149bdc20924df55412a9553e94780924
shivaprasadc09-dev <shivaprasadc09@gmail.com>
2026-05-09 14:20:03 +0530

    additional details

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,6 +1,19 @@
 
 num1= int(input("Enter the first number : "))
 num2= int(input("Enter the second number : "))
+sign = input("+,-,*,/")
+
+if sign == '+':
+    result = num1 + num2
+
+if sign == '-':
+    result = num1 + num2 
+
+if sign == '*':
+    result = num1 * num2 
+
+if sign == '/':
+    result = num1/num2 
 
 num3 = num1+num2
 
